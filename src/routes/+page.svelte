63d812be837eb75ab55a05df06499cd86c60e45f
otcova <otgercomas@gmail.com>
2023-01-04 13:27:01 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let count = 3;
+</script>
+
+<button on:click={() => ++count}>+1</button>
+<div id="count">{count}</div>
+<button on:click={() => --count}>-1</button>
+
+<style>
+	#count {
+		font-size: 30px;
+	}
+</style>
