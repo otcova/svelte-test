4338b2e8a3565fa9600a6572668199d0e530dc95
otcova <otgercomas@gmail.com>
2023-01-10 16:07:05 +0100

    Updates

--- _app/immutable/chunks/2-3d37ffdf.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-1064d8f5.js";export{t as component};
---
+++ _app/immutable/chunks/2-962c6916.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-15e685a0.js";export{t as component};
