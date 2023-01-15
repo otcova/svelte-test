8ccb82b12adc1af87d5da15933cbe18fb57a798e
otcova <otgercomas@gmail.com>
2023-01-15 19:52:19 +0100

    Updates

--- _app/immutable/chunks/1-594105d7.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-2f253e18.js";export{t as component};
---
+++ _app/immutable/chunks/2-2170da49.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-b38ff286.js";export{t as component};
